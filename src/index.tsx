79bd31248d913550b870d8ec0319f2b95ff1de01
0xNotMe <nme15500@gmail.com>
2025-03-31 15:54:48 +1100

    fix: routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import './index.css';
 import App from './App';
+import Login from './pages/Login';
+import CharacterCreation from './pages/CharacterCreation';
+import GameBoard from './pages/GameBoard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +17,11 @@ root.render(
   <React.StrictMode>
     <RecoilRoot>
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/create" element={<ProtectedRoute><CharacterCreation /></ProtectedRoute>} />
+          <Route path="/game" element={<ProtectedRoute><GameBoard /></ProtectedRoute>} />
+        </Routes>
       </BrowserRouter>
     </RecoilRoot>
   </React.StrictMode>
