7996dccfd734abed3dd46ae33da84ba3c160e27f
0xNotMe <nme15500@gmail.com>
2025-04-02 15:56:58 +1100

    fix: next build

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 
 const nextConfig = {
   /* config options here */
-  distDir: 'out',
-  output: 'export',
   // Remove webpack cache files that exceed Cloudflare's 25MB limit
   webpack: (config) => {
     // Don't include large cache files in the output
@@ -13,5 +11,10 @@ const nextConfig = {
     return config;
   },
 };
+
+if (process.env.NODE_ENV === 'production') {
+  nextConfig.distDir = 'out';
+  nextConfig.output = 'export';
+}
 
 module.exports = nextConfig;
