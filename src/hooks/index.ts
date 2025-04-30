abcf9cfef085c562ae8358086d1c80f293adfb5b
0xNotMe <nme15500@gmail.com>
2025-04-30 20:20:12 +1000

    cleanup: removed unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,10 +1,7 @@
 // Game hooks
 export * from './game/useBattleNads';
 export * from './game/useGame';
-export * from './game/useChat';
-export * from './game/useCombat';
 export * from './game/useEquipment';
-export * from './game/useCharacter';
 export * from './game/useUiSnapshot';
 
 // Session hooks
